2492573fbfd70fb01c5cc2d4a00f753944de5533
yxeque <yxequel@gmail.com>
2024-02-02 08:55:06 +0800

    Iteration 0: Loading a file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,13 @@
 puts 'Event Manager Initialized'
+
+# puts File.exist? "event_attendees.csv"
+# contents = File.read('event_attendees.csv')
+# puts contents
+
+lines = File.readlines('event_attendees.csv')
+lines.each do |line|
+  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
