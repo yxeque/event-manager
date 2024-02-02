4dd7c9f7341ed82b8caefc3b90d8a111bf09a853
yxeque <yxequel@gmail.com>
2024-02-02 11:22:59 +0800

    Clean mismatch zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,5 +8,15 @@ contents = CSV.open('event_attendees.csv',
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+
+
   puts "#{name} #{zipcode}"
 end
